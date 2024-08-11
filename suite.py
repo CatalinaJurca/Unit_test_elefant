8bda0714bb1f1ad5a1e7d8f15d07342802a50a54
CatalinaJurca <162631750+CatalinaJurca@users.noreply.github.com>
2024-08-11 22:21:11 +0300

    Update suite.py

--- suite.py
+++ suite.py
@@ -8,8 +8,8 @@ from test_contact_form import Test_Contact_Form
 class TestSuite(unittest.TestCase):
     def test_suite(self):
         test_list = unittest.TestSuite()
-        test_list.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(Test_Search)])
-                            # unittest.defaultTestLoader.loadTestsFromTestCase(Test_Contact_Form)])
+        test_list.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(Test_Search),
+                            unittest.defaultTestLoader.loadTestsFromTestCase(Test_Contact_Form)])
 
         runner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_title="Test execution report",
                                                report_name="Test results")
